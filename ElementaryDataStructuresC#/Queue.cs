3ccd73aaf749833305743c271542dd331113e726
Roman Zinchenko <roman@codemerx.com>
2023-05-05 17:07:50 +0300

    queue, stack

---
+++ ElementaryDataStructuresC#/Queue.cs
@@ -0,0 +1,45 @@
+﻿
+namespace ElementaryDataStructuresCs
+{
+    public class QueueViaTwoStacks<T>
+    {
+        public QueueViaTwoStacks() {
+            s1 = new Stack<T>();
+            s2 = new Stack<T>();
+        }
+
+	    public void Enqueue( T val ) {
+
+		    // перелить из s2 все что есть в s1
+		    while ( s2.Any() ) {
+			    s1.Push(s2.Pop());
+		    }
+
+            // записать новое значение
+            s1.Push(val);
+	    }
+
+        public T Dequeue() {
+
+            // перелить из s1 все что есть в s2
+            while (s1.Any()) {
+                s2.Push(s1.Pop());
+            }
+
+            // вернуть значение            
+            return s2.Pop();
+        }
+
+        public T Peek() {
+            // перелить из s1 все что есть в s2
+            while (s1.Any())
+            {
+                s2.Push(s1.Pop());
+            }
+            return s2.Peek();
+        }
+
+        private readonly Stack<T> s1;
+        private readonly Stack<T> s2;
+    }
+}
---
+++ ElementaryDataStructuresC#/Stack.cs
@@ -0,0 +1,109 @@
+﻿
+namespace ElementaryDataStructuresCs
+{
+    public class StackViaTwoQueues<T>
+    {
+        public StackViaTwoQueues()
+        {
+            q1 = new Queue<T>();
+            q2 = new Queue<T>();
+        }
+
+        public void Push( T val ) {
+
+            Queue<T> q_from;
+            Queue<T> q_to;
+
+            if ( q1.Count < q2.Count ) {
+                q_from = q1;
+                q_to = q2;
+            } else if (q2.Count < q1.Count) {
+                q_from = q2;
+                q_to = q1;
+            } else if (!q1.Any() && !q2.Any()) {
+                q1.Enqueue(val);
+                return;
+            } else {
+                throw new Exception("Must not hit into here!");
+            }
+
+            while (q_from.Count > 0) {
+                q_to.Enqueue(q_from.Dequeue());
+            }
+
+            q_to.Enqueue(val);
+        }
+
+        public T Peek() {
+
+            if (!q1.Any() && !q2.Any()) {
+                throw new Exception("Stack underflow");
+            }
+
+            if (q1.Count == 1) {
+                return q1.Peek();
+            }
+            if (q2.Count == 1) {
+                return q2.Peek();
+            }
+
+            Queue<T> q_from;
+            Queue<T> q_to;
+
+            if ( q1.Count < q2.Count ) {
+                q_from = q2;
+                q_to = q1;
+            }
+            else if (q2.Count < q1.Count ) {
+                q_from = q1;
+                q_to = q2;
+            } else {
+                throw new Exception("Must not hit into here!");
+            }
+
+            while (q_from.Count > 1) {
+                q_to.Enqueue(q_from.Dequeue());
+            }
+            return q_from.Peek();
+        }
+
+        public T Pop() {
+            if (q1.Count == 1) {
+                return q1.Dequeue();
+            }
+            if (q2.Count == 1) {
+                return q2.Dequeue();
+            }
+
+            Queue<T> q_from;
+            Queue<T> q_to;
+            if (q1.Count < q2.Count ) {
+                q_from = q2;
+                q_to = q1;
+            }
+            else if (q2.Count < q1.Count )
+            {
+                q_from = q1;
+                q_to = q2;
+            }
+            else if (!q1.Any() && !q2.Any() )
+            {
+                throw new Exception("Stack underflow");
+            }
+            else
+            {
+                throw new Exception("Must not hit into here!");
+            }
+
+            while (q_from.Count > 1)
+            {
+                q_to.Enqueue(q_from.Dequeue());
+            }
+
+            return q_from.Dequeue();
+        }
+
+        private readonly Queue<T> q1;
+        private readonly Queue<T> q2;
+    }
+}
