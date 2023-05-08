a03b47a1503e42c7f0f25084a3d425845b7cf4da
Roman Zinchenko <roman@codemerx.com>
2023-05-08 22:06:43 +0300

    Boruvka algo refactorings and tests

--- GraphsCs/Boruvka_MST.cs
+++ GraphsCs/Boruvka_MST.cs
@@ -3,106 +3,137 @@ namespace GraphsCs;
 
 public static partial class Lib {
 
+    private static object lockObject = new object();
+
+    /// <summary>
+    /// Boruvka MST algorithm
+    /// </summary>
+    /// <param name="vertices">List of vertices of a graph</param>
+    /// <param name="adjMatrix">Adjacency matrix</param>
+    /// <returns>List of edges of MST in format "u|v"</returns>
     public static ICollection<string> Boruvka_MST(List<Vertex> vertices, int?[,] adjMatrix) {
 
         var dic = new Dictionary<string, int>();
 
         foreach (var vertex in vertices) {
-            _ = GetCheapestEdge(vertex , adjMatrix, dic);
+            _ = GetCheapestEdge(vertex, adjMatrix, dic);
         }
-                
-        var set = new HashSet<int>();
-
-        var exceptList = new HashSet<string>();
-
+   
         while (true) {
-            
-            var seed = dic.Keys.First(x => !exceptList.Contains(x) ).Split('|');
 
-            var frontier = new HashSet<int>() { int.Parse(seed[0]), int.Parse( seed[1] ) };
-        
-            while ( frontier.Any() ) {
+            var set = new HashSet<int>();
+            var exceptList = new HashSet<string>();
+            var components = new List<HashSet<string>>();
 
-                var new_frontier = new HashSet<int>();
+            while (set.Count != vertices.Count) {
 
-                foreach (var item in frontier) {
+                var component = new HashSet<string>();
+                var seed = dic.Keys.First(x => !exceptList.Contains(x)).Split('|');
+                var frontier = new HashSet<int>() { int.Parse(seed[0]), int.Parse(seed[1]) };
+                
+                while (frontier.Any()) {
 
-                    var res = dic.Keys.Where( x => !exceptList.Contains( x ) && x.Contains( item.ToString()) ).ToList();
+                    var new_frontier = new HashSet<int>();
 
-                    foreach ( var key in res ) {
+                    foreach (var item in frontier) {
 
-                        exceptList.Add( key );
+                        var res = dic.Keys.Where(x => !exceptList.Contains(x) && (x.Substring(0, x.IndexOf('|')) == item.ToString() || x.Substring(x.IndexOf('|') + 1) == item.ToString())).ToList();
 
-                        var arr = key.Split('|');
-                        var u = int.Parse(arr[0]);
-                        var v = int.Parse(arr[1]);
+                        foreach (var key in res) {
 
-                        if ( !frontier.Contains( u ) && !set.Contains( u ) ) {
-                            new_frontier.Add( u );
+                            exceptList.Add(key);
+
+                            var arr = key.Split('|');
+                            var u = int.Parse(arr[0]);
+                            var v = int.Parse(arr[1]);
+
+                            component.Add(key);
+
+                            if (!frontier.Contains(u) && !set.Contains(u)) {
+                                new_frontier.Add(u);
+                            }
+
+                            if (!frontier.Contains(v) && !set.Contains(v)) {
+                                new_frontier.Add(v);
+                            }
+
+                            set.Add(u);
+                            set.Add(v);
                         }
-
-                        if ( !frontier.Contains( v ) && !set.Contains( v ) ) {
-                            new_frontier.Add( v );
-                        }
-
-                        set.Add( u );
-                        set.Add( v );
                     }
+                    frontier = new_frontier;
                 }
-                frontier = new_frontier;
+                components.Add(component);
             }
 
-            if ( set.Count == vertices.Count ) {
-                break;
+            if (components.Count == 1 && components[0].Count == vertices.Count - 1) {
+                return dic.Keys.ToArray();
             }
 
-            var collectionOfVertices = vertices.Where(x => set.Contains( x.GetAdjId() )).ToList();
-            _ = GetCheapestEdge(collectionOfVertices, adjMatrix, dic);
+            var tasksList = new List<Task>();
+
+            foreach (var component in components) {
+                
+                var collectionOfVertices = new HashSet<Vertex>();
+
+                foreach (var edge in component) {
+                    var vert1 = vertices.First(x => x.GetAdjId().ToString() == edge.Substring(0, edge.IndexOf('|')));
+                    var vert2 = vertices.First(x => x.GetAdjId().ToString() == edge.Substring(edge.IndexOf('|') + 1));
+                    collectionOfVertices.Add(vert1);
+                    collectionOfVertices.Add(vert2);
+                }
+
+                tasksList.Add(
+                    Task.Factory.StartNew(() => {
+                        _ = GetCheapestEdge(collectionOfVertices, adjMatrix, dic);
+                    }));
+            }
+            Task.WaitAll(tasksList.ToArray());
         }
-
-        return dic.Keys.ToArray();
     }
 
     private static (int, int)? GetCheapestEdge(ICollection<Vertex> vertices, int?[,] adjMatrix, Dictionary<string, int> dic) {
+    
+        lock (lockObject) {
 
-        var weight = int.MaxValue;
-        var adj_id_u = -1;
-        var adj_id_v = -1;
+            var weight = int.MaxValue;
+            var adj_id_u = -1;
+            var adj_id_v = -1;
 
-        foreach (var vertex in vertices) {
+            foreach (var vertex in vertices) {
             
-            var i = vertex.GetAdjId();
+                var i = vertex.GetAdjId();
             
-            for (int j = 0; j < adjMatrix.GetLength(1); j++) {
-            
-                if ( i == j || adjMatrix[i, j] == null) {
-                    continue;
-                }
+                for (int j = 0; j < adjMatrix.GetLength(1); j++) {
+           
+                    if ( i == j || adjMatrix[i, j] == null) {
+                        continue;
+                    }
 
-                if (dic.ContainsKey(i + "|" + j) || dic.ContainsKey(j + "|" + i)) {
-                    continue;
-                }
+                    var t = j;
+                    if ( vertices.Where(x => x.GetAdjId() == i).Any() && vertices.Where( x => x.GetAdjId() == t ).Any() ) {
+                        continue;
+                    }
 
-                if (adjMatrix[i, j] < weight) {
-                    weight = adjMatrix[i, j]!.Value;
-                    adj_id_v = j;
-                    adj_id_u = i;
+                    if (adjMatrix[i, j] < weight) {
+                        weight = adjMatrix[i, j]!.Value;
+                        adj_id_v = j;
+                        adj_id_u = i;
+                    }
                 }
             }
+
+            if (adjMatrix[ adj_id_v, adj_id_u ] == null) {
+                return null;
+            }
+        
+            var k = adj_id_v + "|" + adj_id_u;
+            var kReverse = adj_id_u + "|" + adj_id_v;
+            if ( !dic.ContainsKey( k ) && !dic.ContainsKey( kReverse ) ) {
+                dic.Add( k, weight );
+            }
+            return (adj_id_u, adj_id_v);
         }
-
-        if (adjMatrix[ adj_id_v, adj_id_u ] == null) {
-            return null;
-        }
-
-        var k = adj_id_v + "|" + adj_id_u;
-        var kReverse = adj_id_u + "|" + adj_id_v;
-        if ( !dic.ContainsKey( k ) && !dic.ContainsKey( kReverse ) ) {
-            dic.Add( k, weight );
-        }
-
-        return ( adj_id_u, adj_id_v );
-
     }
 
     private static (int, int)? GetCheapestEdge(Vertex vertex, int?[,] adjMatrix, Dictionary<string, int> dic)
@@ -122,6 +153,7 @@ public static partial class Lib {
                 adj_id_v = j;
             }
 
+            // to avoid cycles
             var key = adj_id_v + "|" + adj_id_u;
             if ( dic.ContainsKey( key ) ) {
                 if ( weight >= dic[ key ] ) {
--- TestingForGraphsCs/Boruvka_MST.cs
+++ TestingForGraphsCs/Boruvka_MST.cs
@@ -31,16 +31,19 @@ namespace TestingForGraphsCs
                 /* H*/  { null, null, 20,null, 13,  null, 18,   null }
             };
 
-            var res = Lib.Boruvka_MST(list, adj_Matrix);
+            for (int i = 0; i < 100; i++)
+            {
+                var res = Lib.Boruvka_MST(list, adj_Matrix);
 
-            Assert.AreEqual(7, res.Count);
-            Assert.IsTrue( res.Contains("1|0"));
-            Assert.IsTrue(res.Contains("2|1"));   
-            Assert.IsTrue(res.Contains("5|3"));
-            Assert.IsTrue(res.Contains("6|4"));
-            Assert.IsTrue(res.Contains("4|7"));
-            Assert.IsTrue(res.Contains("4|2"));
-            Assert.IsTrue(res.Contains("3|1"));
+                Assert.AreEqual(list.Count - 1, res.Count);
+                Assert.IsTrue(res.Contains("1|0") || res.Contains("0|1"));
+                Assert.IsTrue(res.Contains("2|1") || res.Contains("1|2"));
+                Assert.IsTrue(res.Contains("5|3") || res.Contains("3|5"));
+                Assert.IsTrue(res.Contains("6|4") || res.Contains("4|6"));
+                Assert.IsTrue(res.Contains("4|7") || res.Contains("7|4"));
+                Assert.IsTrue(res.Contains("4|2") || res.Contains("2|4"));
+                Assert.IsTrue(res.Contains("3|1") || res.Contains("1|3"));
+            }
         }
 
         [TestMethod]
@@ -69,16 +72,19 @@ namespace TestingForGraphsCs
                 /* H*/  { null, null,null,28, 20,  null, 27,  null }
             };
 
-            var res = Lib.Boruvka_MST(list, adj_Matrix);
+            for (int i = 0; i < 100; i++)
+            {
+                var res = Lib.Boruvka_MST(list, adj_Matrix);
 
-            Assert.AreEqual(7, res.Count);
-            Assert.IsTrue(res.Contains("5|0"));
-            Assert.IsTrue(res.Contains("3|1"));
-            Assert.IsTrue(res.Contains("4|2"));
-            Assert.IsTrue(res.Contains("4|3"));
-            Assert.IsTrue(res.Contains("3|6"));
-            Assert.IsTrue(res.Contains("4|7"));
-            Assert.IsTrue(res.Contains("3|0"));
+                Assert.AreEqual(list.Count - 1, res.Count);
+                Assert.IsTrue(res.Contains("5|0") || res.Contains("0|5"));
+                Assert.IsTrue(res.Contains("3|1") || res.Contains("1|3"));
+                Assert.IsTrue(res.Contains("4|2") || res.Contains("2|4") );
+                Assert.IsTrue(res.Contains("4|3") || res.Contains("3|4"));
+                Assert.IsTrue(res.Contains("3|6") || res.Contains("6|3"));
+                Assert.IsTrue(res.Contains("4|7") || res.Contains("7|4"));
+                Assert.IsTrue(res.Contains("3|0") || res.Contains("0|3"));
+            }
         }
 
         [TestMethod]
@@ -109,17 +115,287 @@ namespace TestingForGraphsCs
                 /* BOS*/  { 2704,null,null,867, 1258,null,187,null, null }
             };
 
-            var res = Lib.Boruvka_MST(list, adj_Matrix);
-            Assert.AreEqual(8, res.Count);
-            Assert.IsTrue(res.Contains("1|0"));
-            Assert.IsTrue(res.Contains("3|2"));
-            Assert.IsTrue(res.Contains("5|3"));
-            Assert.IsTrue(res.Contains("5|4"));
-            Assert.IsTrue(res.Contains("6|5"));
-            Assert.IsTrue(res.Contains("7|6"));
-            Assert.IsTrue(res.Contains("6|8"));
-            Assert.IsTrue(res.Contains("2|1"));
+            for (int i = 0; i < 100; i++)
+            {
+                var res = Lib.Boruvka_MST(list, adj_Matrix);
+
+                Assert.AreEqual(list.Count - 1, res.Count);
+                Assert.IsTrue(res.Contains("1|0") || res.Contains("0|1"));
+                Assert.IsTrue(res.Contains("3|2") || res.Contains("2|3"));
+                Assert.IsTrue(res.Contains("5|3") || res.Contains("3|5"));
+                Assert.IsTrue(res.Contains("5|4") || res.Contains("4|5"));
+                Assert.IsTrue(res.Contains("6|5") || res.Contains("5|6"));
+                Assert.IsTrue(res.Contains("7|6") || res.Contains("6|7"));
+                Assert.IsTrue(res.Contains("6|8") || res.Contains("8|6"));
+                Assert.IsTrue(res.Contains("2|1") || res.Contains("1|2"));
+            }
         }
 
+        [TestMethod]
+        public void Test4() {
+            var list = new List<Vertex>();
+            for (int i = 0; i < 87; i++)
+            {
+                list.Add( new Vertex($"{i}", Guid.NewGuid().ToString(), i) );
+            }
+
+            int?[,] adj_Matrix = new int?[87, 87];
+
+            setMatrix(adj_Matrix, 0, 1, 54);
+            setMatrix(adj_Matrix, 0, 16, 38);
+            setMatrix(adj_Matrix, 1, 2, 64);
+            setMatrix(adj_Matrix, 1, 15, 75);
+            setMatrix(adj_Matrix, 1, 56, 59);
+            setMatrix(adj_Matrix, 2, 3, 57);
+            setMatrix(adj_Matrix, 3, 4, 51);
+            setMatrix(adj_Matrix, 4, 5, 95);
+            setMatrix(adj_Matrix, 4, 8, 57);
+            setMatrix(adj_Matrix, 5, 6, 66);
+            setMatrix(adj_Matrix, 5, 11, 110);
+            setMatrix(adj_Matrix, 6, 7, 63);
+            setMatrix(adj_Matrix, 6, 11, 49);
+            setMatrix(adj_Matrix, 8, 9, 76);
+            setMatrix(adj_Matrix, 8, 12, 48);
+            setMatrix(adj_Matrix, 9, 10, 70);
+            setMatrix(adj_Matrix, 9, 11, 74);
+            setMatrix(adj_Matrix, 10, 12, 129);
+            setMatrix(adj_Matrix, 10, 24, 63);
+            setMatrix(adj_Matrix, 12, 13, 76);
+            setMatrix(adj_Matrix, 12, 15, 86);
+            setMatrix(adj_Matrix, 13, 14, 67);
+            setMatrix(adj_Matrix, 14, 24, 42);
+            setMatrix(adj_Matrix, 16, 17, 64);
+            setMatrix(adj_Matrix, 16, 30, 51);
+            setMatrix(adj_Matrix, 17, 18, 80);
+            setMatrix(adj_Matrix, 17, 27, 77);
+            setMatrix(adj_Matrix, 18, 19, 72);
+            setMatrix(adj_Matrix, 19, 20, 45);
+            setMatrix(adj_Matrix, 20, 21, 76);
+            setMatrix(adj_Matrix, 20, 25, 82);
+            setMatrix(adj_Matrix, 20, 26, 47);
+            setMatrix(adj_Matrix, 21, 22, 51);
+            setMatrix(adj_Matrix, 23, 24, 88);
+            setMatrix(adj_Matrix, 23, 25, 76);
+            setMatrix(adj_Matrix, 23, 29, 48);
+            setMatrix(adj_Matrix, 27, 28, 73);
+            setMatrix(adj_Matrix, 28, 29, 62);
+            setMatrix(adj_Matrix, 30, 31, 50);
+            setMatrix(adj_Matrix, 30, 44, 101);
+            setMatrix(adj_Matrix, 31, 32, 49);
+            setMatrix(adj_Matrix, 31, 53, 75);
+            setMatrix(adj_Matrix, 32, 33, 54);
+            setMatrix(adj_Matrix, 32, 50, 61);
+            setMatrix(adj_Matrix, 33, 34, 59);
+            setMatrix(adj_Matrix, 34, 35, 41);
+            setMatrix(adj_Matrix, 34, 38, 69);
+            setMatrix(adj_Matrix, 35, 36, 58);
+            setMatrix(adj_Matrix, 36, 37, 41);
+            setMatrix(adj_Matrix, 36, 40, 41);
+            setMatrix(adj_Matrix, 36, 43, 33);
+            setMatrix(adj_Matrix, 38, 39, 65);
+            setMatrix(adj_Matrix, 39, 40, 45);
+            setMatrix(adj_Matrix, 39, 41, 74);
+            setMatrix(adj_Matrix, 41, 42, 52);
+            setMatrix(adj_Matrix, 42, 43, 33);
+            setMatrix(adj_Matrix, 44, 45, 58);
+            setMatrix(adj_Matrix, 45, 46, 80);
+            setMatrix(adj_Matrix, 45, 53, 85);
+            setMatrix(adj_Matrix, 45, 78, 65);
+            setMatrix(adj_Matrix, 45, 85, 63);
+            setMatrix(adj_Matrix, 46, 47, 60);
+            setMatrix(adj_Matrix, 47, 48, 89);
+            setMatrix(adj_Matrix, 47, 51, 100);
+            setMatrix(adj_Matrix, 48, 49, 74);
+            setMatrix(adj_Matrix, 50, 51, 60);
+            setMatrix(adj_Matrix, 51, 52, 48);
+            setMatrix(adj_Matrix, 51, 54, 93);
+            setMatrix(adj_Matrix, 54, 55, 48);
+            setMatrix(adj_Matrix, 56, 57, 94);
+            setMatrix(adj_Matrix, 57, 58, 51);
+            setMatrix(adj_Matrix, 57, 60, 77);
+            setMatrix(adj_Matrix, 57, 74, 83);
+            setMatrix(adj_Matrix, 58, 59, 62);
+            setMatrix(adj_Matrix, 58, 64, 78);
+            setMatrix(adj_Matrix, 58, 66, 66);
+            setMatrix(adj_Matrix, 60, 61, 79);
+            setMatrix(adj_Matrix, 60, 71, 54);
+            setMatrix(adj_Matrix, 61, 62, 69);
+            setMatrix(adj_Matrix, 61, 65, 45);
+            setMatrix(adj_Matrix, 62, 63, 56);
+            setMatrix(adj_Matrix, 62, 68, 50);
+            setMatrix(adj_Matrix, 64, 65, 59);
+            setMatrix(adj_Matrix, 66, 67, 81);
+            setMatrix(adj_Matrix, 68, 69, 58);
+            setMatrix(adj_Matrix, 69, 70, 63);
+            setMatrix(adj_Matrix, 70, 86, 68);
+            setMatrix(adj_Matrix, 71, 72, 54);
+            setMatrix(adj_Matrix, 71, 76, 63);
+            setMatrix(adj_Matrix, 72, 73, 56);
+            setMatrix(adj_Matrix, 73, 80, 57);
+            setMatrix(adj_Matrix, 73, 76, 70);
+            setMatrix(adj_Matrix, 73, 81, 55);
+            setMatrix(adj_Matrix, 74, 75, 55);
+            setMatrix(adj_Matrix, 74, 76, 57);
+            setMatrix(adj_Matrix, 75, 78, 52);
+            setMatrix(adj_Matrix, 76, 77, 44);
+            setMatrix(adj_Matrix, 77, 80, 66);
+            setMatrix(adj_Matrix, 78, 79, 70);
+            setMatrix(adj_Matrix, 79, 80, 45);
+            setMatrix(adj_Matrix, 81, 82, 52);
+            setMatrix(adj_Matrix, 81, 86, 54);
+            setMatrix(adj_Matrix, 82, 83, 60);
+            setMatrix(adj_Matrix, 83, 84, 58);
+            setMatrix(adj_Matrix, 83, 85, 60);
+
+            var res = Lib.Boruvka_MST(list, adj_Matrix);
+            Assert.AreEqual(list.Count - 1, res.Count);
+
+        }
+
+        [TestMethod]
+        public void Test5() {
+
+            //https://www.geeksforgeeks.org/boruvkas-algorithm-greedy-algo-9/
+
+            var vertexA = new Vertex("0", Guid.NewGuid().ToString(), 0);
+            var vertexB = new Vertex("1", Guid.NewGuid().ToString(), 1);
+            var vertexC = new Vertex("2", Guid.NewGuid().ToString(), 2);
+            var vertexD = new Vertex("3", Guid.NewGuid().ToString(), 3);
+            var vertexE = new Vertex("4", Guid.NewGuid().ToString(), 4);
+            var vertexF = new Vertex("5", Guid.NewGuid().ToString(), 5);
+            var vertexG = new Vertex("6", Guid.NewGuid().ToString(), 6);
+            var vertexH = new Vertex("7", Guid.NewGuid().ToString(), 7);
+            var vertexI = new Vertex("8", Guid.NewGuid().ToString(), 8);
+
+            var list = new List<Vertex> { vertexA, vertexB, vertexC, vertexD, vertexE, vertexF, vertexG, vertexH, vertexI };
+
+            int?[,] adj_Matrix = new int?[9, 9] { 
+                        // 0    1     2    3     4     5     6     7   8
+                /* 0*/  { null, 4,  null, null, null, null, null, 8, null },
+                /* 1*/  { 4,   null, 8,   null, null, null, null, 11, null },
+                /* 2*/  { null, 8,  null,  7,    null, 4,   null,  null, 2 },
+                /* 3*/  { null, null, 7, null,   9,    14,  null, null, null },
+                /* 4*/  { null, null,null, 9,  null,  10,    null, null, null },
+                /* 5*/  { null,null, 4,    14,  10,  null,   2,   null, null  },
+                /* 6*/  { null,null, null,null,  null, 2,  null, 1, 6 },
+                /* 7*/  {   8,  11,  null,null, null,null,   1,   null, 7 },
+                /* 8*/  { null, null, 2, null, null, null,   6,  7, null }
+            };
+
+            for (int i = 0; i < 100; i++)
+            {
+                var res = Lib.Boruvka_MST(list, adj_Matrix);
+
+                Assert.AreEqual(8, res.Count);
+                Assert.IsTrue(res.Contains("1|0") || res.Contains("0|1"));
+                Assert.IsTrue(res.Contains("8|2") || res.Contains("2|8"));
+                Assert.IsTrue(res.Contains("2|3") || res.Contains("3|2"));
+                Assert.IsTrue(res.Contains("3|4") || res.Contains("4|3"));
+                Assert.IsTrue(res.Contains("6|5") || res.Contains("5|6"));
+                Assert.IsTrue(res.Contains("7|6") || res.Contains("6|7"));
+                Assert.IsTrue(res.Contains("2|1") || res.Contains("1|2"));
+                Assert.IsTrue(res.Contains("5|2") || res.Contains("2|5"));
+            }
+        }
+
+        [TestMethod]
+        public void Test6() {
+
+            var vertexA = new Vertex("A", Guid.NewGuid().ToString(), 0);
+            var vertexB = new Vertex("B", Guid.NewGuid().ToString(), 1);
+            var vertexC = new Vertex("C", Guid.NewGuid().ToString(), 2);
+            var vertexD = new Vertex("D", Guid.NewGuid().ToString(), 3);
+            var vertexE = new Vertex("E", Guid.NewGuid().ToString(), 4);
+            var vertexF = new Vertex("F", Guid.NewGuid().ToString(), 5);
+            var vertexG = new Vertex("G", Guid.NewGuid().ToString(), 6);
+            var vertexH = new Vertex("H", Guid.NewGuid().ToString(), 7);
+
+            int?[,] adj_Matrix = new int?[8, 8] { 
+                        // A    B    C    D     E    F     G     H
+                /* A*/  { null, 1,  null, 1, null, null, null, null },
+                /* B*/  { 1,   null, 1,   1,  1,   null, null, null },
+                /* C*/  { null, 1, null, null, 1,   null, null,  1 },
+                /* D*/  { 1,   1, null, null,null,  1,    1,  null },
+                /* E*/  { null, 1,  1,   null,null, null,  1,  1 },
+                /* F*/  { null,null, null, 1,  null, null, 1,  null },
+                /* G*/  { null,null, null,1,  1,    1,  null, 1 },
+                /* H*/  { null, null, 1,null, 1,  null, 1,   null }
+            };
+
+            var list = new List<Vertex> { vertexA, vertexB, vertexC, vertexD, vertexE, vertexF, vertexG, vertexH };
+
+            for (int i = 0; i < 100; i++)
+            {
+                var res = Lib.Boruvka_MST(list, adj_Matrix);
+
+                Assert.AreEqual(list.Count - 1, res.Count);
+                Assert.IsTrue(res.Contains("1|0"));
+                Assert.IsTrue(res.Contains("1|2"));
+                Assert.IsTrue(res.Contains("0|3"));
+                Assert.IsTrue(res.Contains("1|4"));
+                Assert.IsTrue(res.Contains("3|5"));
+                Assert.IsTrue(res.Contains("3|6"));
+                Assert.IsTrue(res.Contains("2|7"));
+            }
+        }
+
+        [TestMethod]
+        public void Test7() {
+
+            var list = new List<Vertex>();
+            for (int i = 0; i < 16; i++)
+            {
+                list.Add(new Vertex($"{i}", Guid.NewGuid().ToString(), i));
+            }
+
+            int?[,] adj_Matrix = new int?[16, 16];
+
+            setMatrix(adj_Matrix, 0, 1, 54);
+            setMatrix(adj_Matrix, 0, 13, 38);
+            setMatrix(adj_Matrix, 1, 2, 64);
+            setMatrix(adj_Matrix, 1, 14, 75);
+            setMatrix(adj_Matrix, 1, 15, 59);
+            setMatrix(adj_Matrix, 2, 3, 57);
+            setMatrix(adj_Matrix, 3, 4, 51);
+            setMatrix(adj_Matrix, 4, 5, 95);
+            setMatrix(adj_Matrix, 4, 8, 57);
+            setMatrix(adj_Matrix, 5, 6, 66);
+            setMatrix(adj_Matrix, 5, 11, 110);
+            setMatrix(adj_Matrix, 6, 7, 63);
+            setMatrix(adj_Matrix, 6, 11, 49);
+            setMatrix(adj_Matrix, 8, 9, 76);
+            setMatrix(adj_Matrix, 8, 12, 48);
+            setMatrix(adj_Matrix, 9, 10, 70);
+            setMatrix(adj_Matrix, 9, 11, 74);
+            setMatrix(adj_Matrix, 10, 12, 129);
+
+            for (int i = 0; i < 100; i++)
+            {
+                var res = Lib.Boruvka_MST(list, adj_Matrix);
+
+                Assert.AreEqual(list.Count - 1, res.Count);
+                Assert.IsTrue(res.Contains("13|0") || res.Contains("0|13"));
+                Assert.IsTrue(res.Contains("0|1") || res.Contains("1|0"));
+                Assert.IsTrue(res.Contains("3|2") || res.Contains("2|3"));
+                Assert.IsTrue(res.Contains("4|3") || res.Contains("3|4"));
+                Assert.IsTrue(res.Contains("6|5") || res.Contains("5|6"));
+                Assert.IsTrue(res.Contains("11|6") || res.Contains("6|11"));
+                Assert.IsTrue(res.Contains("6|7") || res.Contains("7|6"));
+                Assert.IsTrue(res.Contains("12|8") || res.Contains("8|12"));
+                Assert.IsTrue(res.Contains("10|9") || res.Contains("9|10"));
+                Assert.IsTrue(res.Contains("1|14") || res.Contains("14|1"));
+                Assert.IsTrue(res.Contains("1|15") || res.Contains("15|1"));
+                Assert.IsTrue(res.Contains("2|1") || res.Contains("1|2"));
+                Assert.IsTrue(res.Contains("8|4") || res.Contains("4|8"));
+                Assert.IsTrue(res.Contains("9|11") || res.Contains("11|9"));
+                Assert.IsTrue(res.Contains("9|8") || res.Contains("8|9"));
+            }
+        }
+
+        private void setMatrix(int?[,] adj_Matrix, int i, int j, int w) {
+            adj_Matrix[i, j] = w;
+            adj_Matrix[j, i] = w;
+        }
+        
     }
 }
